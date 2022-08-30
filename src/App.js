29d70d4501d44a07861686b67efed4e2392d2368
Srejon Khan <srejonkhan79@gmail.com>
2022-08-31 02:53:39 +0600

    update app name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 className="text-3xl font-bold">React is working.</h1>
+        <h1 className="text-3xl font-bold">Note Brew.</h1>
       </div>
     </StrictMode>
   );
